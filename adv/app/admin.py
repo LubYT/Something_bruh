69bf61e8f1b652230a31d28d03f117fc8edb52fa
LubYT <lubister.vip@mail.ru>
2023-08-09 10:29:55 +0300

    Домашка за 8 занятие(Поправил 2 задание)

--- adv/app/admin.py
+++ adv/app/admin.py
@@ -4,7 +4,7 @@ from .models import Advertisements
 
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id','title','description','price','created_at','updated_at','auction']
+    list_display = ['id','title','description','price','created_date','updated_date','auction']
     list_filter = ['auction','created_at','updated_at']
     actions=['make_auction_as_false']
     fieldsets=(
