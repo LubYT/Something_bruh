facee802339074d6eb7ea94ba0e3ccf7243ecad7
LubYT <lubister.vip@mail.ru>
2023-08-29 17:51:01 +0300

    Домашка за 9 занятие

--- adv/app/models.py
+++ adv/app/models.py
@@ -1,48 +1,49 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib import admin
-class Advertisements(models.Model):
+from django.utils.html import format_html
+from django.utils import timezone
 
-    title= models.CharField(
-        max_length=100,
-        verbose_name='название'
-    )
+User = get_user_model()
 
-    description=models.TextField(
-        verbose_name='Описание'
-    )
 
-    price=models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        verbose_name='цена'
-    )
-
-    auction = models.BooleanField(
-        default=False,
-        verbose_name='торг'
-    )
-
-    created_at = models.DateTimeField(auto_now_add=True,verbose_name='Дата публикации')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+class Advertisement(models.Model):
+    title = models.CharField("заголовок", max_length=128)
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    image = models.ImageField("изображение", upload_to="advertisements/")
 
     @admin.display(description='дата создания')
     def created_date(self):
-        from django.utils import timezone
-        from django.utils.html import format_html
-        if self.created_at.date()==timezone.now().date():
-            created_time=self.created_at.time().strftime('%H:%M:%S')
-            return  format_html('<span>Сегодня в {}</span>',created_time)
-        return self.created_at.strftime("%Y-%m-%d %H:%M")
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
 
-    @admin.display(description='дата обновления')
+    @admin.display(description='дата последнего обновления')
     def updated_date(self):
-        from django.utils import timezone
-        from django.utils.html import format_html
         if self.updated_at.date() == timezone.now().date():
-            updated_time = self.updated_at.time().strftime('%H:%M:%S')
-            return format_html('<span style="color: red;">Сегодня в {}</span>', updated_time)
-        return self.updated_at.strftime("%Y-%m-%d %H:%M")
+            created_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                ' <img src="{url}" style="max-width: 80px; max-height: 80px;"', url=self.image.url
+            )
+
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
     class Meta:
-        db_table = 'Advertisements'
+        db_table = "advertisements"
